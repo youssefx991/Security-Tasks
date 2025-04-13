1873c621b3f4c242d8dfce8b71cbd79f7f7d863a
Youssef Wagih <youssefwagih212@gmail.com>
2025-04-13 17:48:57 +0200

    Finish Columnar task

--- Task2/src/main/java/Security/ColumnarCipher.java
+++ Task2/src/main/java/Security/ColumnarCipher.java
@@ -5,8 +5,72 @@ public class ColumnarCipher {
 
     public List<Integer> analyse(String plainText, String cipherText) {
         // TODO: Analyze the plainText and cipherText to determine the key(s)
+        int ctSize = cipherText.length();
+        int ptSize = plainText.length();
+        char[][] grid;
+        List<Integer> key = new ArrayList<>();
+        boolean foundKey = false;
+        for(int rows=2, cols; rows<=ctSize/2; ++rows)
+        {
+            if(ctSize % rows != 0)
+                continue;
 
-        return new ArrayList<>(); // Placeholder return
+            int count = 0;
+            cols = ctSize / rows;
+            grid = new char[rows][cols];
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    if (count >= ptSize) {
+                        grid[i][j] = 'x';
+                    } else {
+                        grid[i][j] = plainText.charAt(count++);
+                    }
+                }
+            }
+
+            // print grid
+            // for (int i = 0; i < rows; i++) {
+            //     for (int j = 0; j < cols; j++) {
+            //         System.out.print(grid[i][j]);
+            //     }
+            //     System.out.println();
+            // }
+            // key = new ArrayList();
+            key.clear();
+            StringBuilder colString = new StringBuilder();
+            foundKey = true;
+
+            for (int j = 0; j < cols; j++) {
+                for (int i = 0; i < rows; i++) {
+                    colString.append(grid[i][j]);
+                }
+
+                // print colString
+                // System.out.println("ColsString: " + colString.toString());
+                int index = cipherText.indexOf(colString.toString());
+
+                if (index != -1) {
+                    // System.out.println("Found at index: " + index + "\t rows: " + rows + "\t number of column: " + ((index/rows) + 1));
+                    key.add((index/rows) + 1);
+                }
+                else {
+                    foundKey = false;
+                    break;
+                }
+
+                colString.setLength(0); // Clear the StringBuilder for the next column
+            }
+
+            if(foundKey)
+                break;
+
+        }
+
+        // System.out.print("key is: ");
+        // for(int i=0; i<key.size(); ++i) {
+        //     System.out.print(key.get(i) + " ");
+        // }
+        return key; // Placeholder return
     }
 
     public String decrypt(String cipherText, List<Integer> key) {
